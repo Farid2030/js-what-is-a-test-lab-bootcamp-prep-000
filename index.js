1f232df40a9cdc4ce13925ea8b905d6f4fecdd98
Farid Mansour Thabet Alabsi <alabsifarid@gmail.com>
2018-06-24 11:19:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // write your code below!
 var name = "Susan"
 var height = 69
-var message = ` is ${height} inches tall` to include 'Susan'
-
+var message = ` is ${height} inches tall to include ${name}
 module.exports = {name, height, message}
